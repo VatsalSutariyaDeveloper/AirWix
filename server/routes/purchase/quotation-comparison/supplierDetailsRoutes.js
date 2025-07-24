d2c4a61274c1458c61007bf050505a223f3de500
DeveloperVivekPatel <DeveloperVivekPatel1@gmail.com>
2025-07-24 14:37:52 +0530

    'quotation comparison working part 2'

--- server/routes/purchase/quotation-comparison/supplierDetailsRoutes.js
+++ server/routes/purchase/quotation-comparison/supplierDetailsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("../../../controllers/purchase/quotation-comparison/supplierDetailsController");
 
 router.post("/", controller.create);
-router.post('/upload-quotation', controller.uploadAndCreateSupplierDetails);
+// router.post('/upload-quotation', controller.uploadAndCreateSupplierDetails);
 router.get("/", controller.getAll);
 router.get("/:id", controller.getById);
 router.put("/:id", controller.update);
